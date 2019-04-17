bf496d13d164ace02da9c74c0f33b1cba487f145
AntoniaSaez <asaez1@miuandes.cl>
2019-04-17 11:42:14 -0400

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,17 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-	#complete this method
+    @_current_user ||= session[:current_user_id] &&
+        User.find_by(id: session[:current_user_id])
+    end
   end
 
   def is_user_logged_in?
 	#complete this method
   	logged_in = false
-	if logged_in then true else redirect_to root_path end 
+  if session[:current_user_id].presence? && session[:current_user_id].notnill?
+    logged_in = true
+  end 
+	if logged_in then true else redirect_to root_path end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
+	cookie[:users_id] = :users_id
 	def new
 	end
 
 	def create
-		#complete this method
+		if user = User.authenticate(params[:username], params[:password])
+      # Save the user ID in the session so it can be used in
+      # subsequent requests
+      session[:current_user_id] = user.id
+			flash[:notice] = ""
+      redirect_to root_path
+    end
+	else
+		flash[:notice] = ""
 	end
 
 	def destroy
